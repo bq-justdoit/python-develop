95aaebe542217b96636295bd6acb5105fde2ffd4
JieQing-Wang <stephency@foxmail.com>
2024-10-14 20:05:43 +0800

    开发控制仪表发射LTE波形函数 完成

--- autotest/transmit_handler.py
+++ autotest/transmit_handler.py
@@ -13,22 +13,21 @@ from instrument_control import InstrumentControl
 class TransmitHandler():
     def __init__(self):
         self.instrument_control = InstrumentControl()
+        self.instrument_control.connect()
 
     def transmit_close(self):
         self.instrument_control.write("CONFigure:SOURce:STATe OFF")
 
-    def transmit_cw_signals(self, rf_port,frequency, power):
+    def transmit_cw_signals(self, rf_port, frequency, power):
         self.frequency = frequency
         self.power = power
-        self.rf_port=rf_port
-        self.instrument_control.connect()
+        self.rf_port = rf_port
 
         # 设置端口
         # 设置端口
         self.instrument_control.write("CONFigure:SOURce:ROUTe:SCENario:SALone RF18")
         self.instrument_control.write("CONFigure:SOURce:ROUTe:USAGe:ALL RF18,OFF,OFF,OFF,OFF,OFF,OFF,OFF,OFF")
         self.instrument_control.write(f"CONFigure:SOURce:ROUTe:USAGe {self.rf_port},ON")
-
 
         # 设置baseband mode 、list模式、external attenuation、dgai
         self.instrument_control.write(command="CONFigure:SOURce:BBMode CW")
@@ -47,12 +46,11 @@ class TransmitHandler():
         print(f"CW信号已发射：频率 = {self.frequency} Hz, 功率 = {self.power} dBm")
 
     # 控制仪表发射LTE波形函数
-    def transmit_arb_signals(self, rf_port,frequency, power):
-
+    def transmit_arb_signals(self, rf_port, frequency, power, arb_file):
         # 设置端口
         self.instrument_control.send("CONFigure:SOURce:ROUTe:SCENario:SALone RF18")
         self.instrument_control.send("CONFigure:SOURce:ROUTe:USAGe:ALL RF18,OFF,OFF,OFF,OFF,OFF,OFF,OFF,OFF")
-        self.instrument_control.send("ONFigure:SOURce:ROUTe:USAGe RF1,ON")
+        self.instrument_control.send(f"CONFigure:SOURce:ROUTe:USAGe {rf_port},ON")
 
         # 设置baseband mode 、list模式、external attenuation、dgain
         self.instrument_control.send("CONFigure:SOURce:BBMode ARB")
@@ -61,11 +59,11 @@ class TransmitHandler():
         self.instrument_control.send("CONFigure:SOURce:RFSettings:DGAin 0")
 
         # 设置frequency、level
-        self.instrument_control.send("CONFigure:SOURce:RFSettings:FREQuency 1.000000E+009")
-        self.instrument_control.send("CONFigure:SOURce:RFSettings:LEVel -70")
+        self.instrument_control.send(f"CONFigure:SOURce:RFSettings:FREQuency {frequency}")
+        self.instrument_control.send(f"CONFigure:SOURce:RFSettings:LEVel {power}")
 
         # Load ARB波形，设置repetition，查询波形属性
-        self.instrument_control.send("CONFigure:SOURce:ARB:FILE 'LTE_20MHz_64QAM_full_UL.wv'")
+        self.instrument_control.send(f"CONFigure:SOURce:ARB:FILE '{arb_file}'")
         self.instrument_control.send("CONFigure:SOURce:ARB:FILE?")
         self.instrument_control.send("CONFigure:SOURce:ARB:REPetition CONT")
         self.instrument_control.send("CONFigure:SOURce:ARB:FILE:DATE?")
@@ -84,13 +82,16 @@ class TransmitHandler():
         # 查询可靠性
         self.instrument_control.send("CONFigure:SOURce:RELiability:ALL?")
 
-        # 关闭Generator
-        self.instrument_control.send("CONFigure:SOURce:STATe OFF")
-
-
+        print(f"CW信号已发射：频率 = {frequency} Hz, 功率 = {power} dBm")
 
 
 if __name__ == '__main__':
-    cw_signals=TransmitHandler()
-    cw_signals.transmit_cw_signals("RF6","1.2E+9", "-30")
-
+    # cw_signals=TransmitHandler()
+    # cw_signals.transmit_cw_signals("RF6","1.2E+9", "-30")
+    arb_signals = TransmitHandler()
+    arb_file = "MTK_LTE_FDD_5M_OCNG.wv"
+    # arb_signals.instrument_control.connect()
+    arb_signals.transmit_arb_signals("RF6", "1.2E+9", "-30", arb_file)
+    arb_signals.instrument_control.send("CONFigure:SOURce:STATe?")
+    arb_signals.instrument_control.send("SYSTem:ERRor:ALL?")
+    arb_signals.instrument_control.close()
